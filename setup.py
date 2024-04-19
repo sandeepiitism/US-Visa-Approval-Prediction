6d1b89b333e13cbe9275382b0c6d7cbc81431c9a
Sandeep <sandeepiit.ism@gmail.com>
2024-04-19 11:27:27 +0530

    notebook and MongoDB setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="SandeepChowdhury",
+    author_email="sandeepiit.ism@gmail.com",
+    packages=find_packages()
+)
