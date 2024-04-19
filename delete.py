56d3dc06854c478d12070df5b8f1eb93692588ed
Sandeep <sandeepiit.ism@gmail.com>
2024-04-19 12:28:22 +0530

    exception.py file updated

--- delete.py
+++ delete.py
@@ -1,3 +1,15 @@
-from us_visa.logger import logging
+import os
+import sys
 
-logging.info("This is test for logging working")
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+## logger test
+# logging.info("This is test for logging working")
+
+## exception test
+try:
+    a = 1/"10"
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e,sys) from e
